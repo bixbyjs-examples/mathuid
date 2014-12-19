6da00b50f8eed5361cb34a3c4502129e2cc16307
Jared Hanson <jaredhanson@gmail.com>
2014-12-19 11:23:18 -0800

    Clarify handler names.

--- app/routes.js
+++ app/routes.js
@@ -4,10 +4,10 @@
 exports = module.exports = function(IoC) {
 
   this.get('/add', IoC.create('handlers/add/show'));
-  this.post('/add', IoC.create('handlers/add/create'));
+  this.post('/add', IoC.create('handlers/add/calc'));
   
   this.get('/sub', IoC.create('handlers/sub/show'));
-  this.post('/sub', IoC.create('handlers/sub/create'));
+  this.post('/sub', IoC.create('handlers/sub/calc'));
   
 };
 
